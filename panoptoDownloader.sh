27c3bffa91ca7cb669ecdf929976fe962bb484f9
Dapizz01 <75532982+Dapizz01@users.noreply.github.com>
2021-02-15 17:19:41 +0100

    Delete panoptoDownloader.sh

--- panoptoDownloader.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while IFS= read -r line
-do
-	link=$(curl $line | pup 'meta[name="twitter:player:stream"] attr{content}')
-	title=$(curl $line | pup 'meta[property="og:title"] attr{content}')
-	$(wget -O $2/"$title" $link)
-done < "$1"
